2cc555e4a1b8d41beb189ad9074cbfab4f8f8d29
DismontHero <shin195802@gmail.com>
2025-10-31 23:21:37 +0300

    Перестройка структуры Generator OpenSearch

---
+++ network.py
@@ -0,0 +1,28 @@
+import opensearchpy
+
+# --- Libraries ---
+
+def initialization_opensearch():
+    pass
+
+
+
+def connect_opensearch():
+    pass
+
+
+
+def create_index_opensearch():
+    pass
+
+
+
+def delete_index_opensearch():
+    pass
+
+
+
+def insert_index_opensearch():
+    pass
+
+
